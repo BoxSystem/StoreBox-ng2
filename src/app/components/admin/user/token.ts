9ca69fb5dc9b0b583d1ed576b68f54bf3856379e
sam <liangguoxian@xcloud.cc>
2018-02-08 14:07:37 +0800

    用户管理增加token管理功能

--- src/app/components/admin/user/token.ts
+++ src/app/components/admin/user/token.ts
@@ -18,30 +18,20 @@ export class TokenComponent implements OnInit {
     _total = 1
     _current = 1
     isVisible = false
-    activeUser: any
-    curUser: any
-    pwdGroup: {
-        old: string,
-        new: string,
-        confirm: string,
-    }
     validateForm: FormGroup
-    hideAddForm: boolean
+    uid: string
     constructor(
         private user: UserService,
         private fb: FormBuilder,
         private _message: NzMessageService,
         private fn: FnService,
         private acRoute: ActivatedRoute
-    ) {
-        this.hideAddForm = true
-        this.curUser = this.fn.getUserSess()
-        this._resetPwdGroup()
-    }
+    ) {}
     private _refreshList() {
         this.acRoute.paramMap.map((params) => {
             return params.get('id')
         }).subscribe((uid) => {
+            this.uid = uid
             this.user.getTokens(uid).subscribe((data: any) => {
                 this._loading = false
                 this._dataSet = data.data
@@ -51,62 +41,14 @@ export class TokenComponent implements OnInit {
     }
     ngOnInit() {
         this._refreshList()
-        this.validateForm = this.fb.group({
-            oldPwd: [this.pwdGroup.old, [Validators.required]],
-            newPwd: [this.pwdGroup.new, [Validators.required]],
-            confirmPwd: [this.pwdGroup.confirm, [
-                Validators.required,
-                this._confirmationValidator
-            ]]
-        })
     }
-    private _confirmationValidator = (control: AbstractControl) => {
-        let bool: boolean = this.validateForm && control.value !== this.validateForm.controls['newPwd'].value
-        return bool ? { confirm: true } : null;
-    };
     del(data) {
-        this.user.del(data._id).subscribe(() => {
+        this.user.delToken(this.uid, data._id).subscribe(() => {
             this._message.success('删除成功！')
             this._refreshList()
         })
     }
-    allow(data) {
-        this.user.allow(data._id).subscribe(() => data.active = true)
-    }
-    ban(data) {
-        this.user.ban(data._id).subscribe(() => data.active = false)
-    }
-    _resetPwdGroup() {
-        this.pwdGroup = {
-            old: '',
-            new: '',
-            confirm: ''
-        }
-    }
-    showPwdFormModal(data) {
-        this._resetPwdGroup()
-        this.isVisible = true
-        this.activeUser = data
-    }
-    changePwd() {
-        this.user.changePassword(
-            this.activeUser._id, this.pwdGroup.old, this.pwdGroup.new
-        ).subscribe((data) => {
-            console.log(data)
-            // this.isVisible = false
-        })
-    }
-    handleCancel = (e) => {
-        console.log(e);
-        this.isVisible = false;
-    }
     getFormControl(name) {
         return this.validateForm.controls[name];
-    }
-    addEvent(status: boolean) {
-        this.hideAddForm = true
-        if (status) {
-            this._refreshList()
-        }
     }
 }
