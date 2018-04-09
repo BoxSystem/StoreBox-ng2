58bee5c6aad68e4c064bed3f88c7bb42fba710d9
sam <liangguoxian@xcloud.cc>
2018-04-09 17:39:29 +0800

    改进SamPasswordEyeComponent组件
    - why: 组件没法使用formControl组件
    - how: 让SamPasswordEyeComponent实现ControlValueAccessor接口

--- src/app/components/password-eye/index.ts
+++ src/app/components/password-eye/index.ts
@@ -1,35 +1,63 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
     styleUrls: ['./index.css'],
     selector: 'app-password-eye',
-    templateUrl: './index.html'
+    templateUrl: './index.html',
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => SamPasswordEyeComponent),
+            multi: true
+        }
+    ]
 })
-export class SamPasswordEyeComponent {
-    @Input() value: any;
-    @Output() valueChange = new EventEmitter();
+export class SamPasswordEyeComponent implements ControlValueAccessor {
+    private _value: any;
     @Input() size: string;
-    @Input() name: string;
-    @Input() PlaceHolder: string;
-    public icon = 'anticon-eye';
-    public type = 'password';
+    @Input() placeholder: string;
     private passwordType = 'password';
     private textType = 'text';
+    public type = this.passwordType;
     private closeEye = 'anticon-eye';
     private openEye = `${this.closeEye}-o`;
+    public iconClass = this.closeEye;
     constructor() {
         this.size = this.size ? this.size : 'large';
     }
-    emitVal() {
-        this.valueChange.emit(this.value);
+    get value() {
+        return this._value;
+    }
+    set value(v) {
+        if (this._value !== v) {
+            this._value = v;
+            this.onChange(this._value);
+        }
     }
     toggle() {
         if (this.type === this.passwordType) {
             this.type = this.textType;
-            this.icon = this.openEye;
+            this.iconClass = this.openEye;
         } else {
             this.type = this.passwordType;
-            this.icon = this.closeEye;
+            this.iconClass = this.closeEye;
         }
+    }
+    onChange = (_: any) => { }
+    onTouched = (_: any) => { }
+    // From ControlValueAccessor interface
+    writeValue(value: any) {
+        this._value = value;
+    }
+
+    // From ControlValueAccessor interface
+    registerOnChange(fn: any) {
+        this.onChange = fn;
+    }
+
+    // From ControlValueAccessor interface
+    registerOnTouched(fn: any) {
+        this.onTouched = fn;
     }
 }
